599049e781a34e01685b58d267da31615c6cf8d8
Morgan Wilcox <31056643+mwilcox6@users.noreply.github.com>
2026-08-23 17:22:57 -0400

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -27,4 +27,45 @@ for ( let i = 0; i < skills.length ;i++) {
 
     skillsList.appendChild(skill)
 }
+let messageForm = document.querySelector('form[name="leave_message"]');
+messageForm.addEventListener("submit" , function (event)
+{
+
+    event.preventDefault();
+    let inputName = event.target.usersName.value;
+    let inputEmail = event.target.usersEmail.value;
+    let inputMessage = event.target.usersMessage.value;
+
+    console.log(inputName);
+    console.log(inputEmail);
+    console.log(inputMessage);
+
+    let messageSection = document.getElementById('messages');
+    let newMessage = document.createElement('li');
+    let messageList = messageSection.querySelector('ul');
+
+    newMessage.innerHTML =  '<a href = "mailto:' +  inputEmail +  '">' +  inputName  +  '</a>' +  "" + '<span>' +  inputMessage  + '</span>'
+
+    let removeButton = document.createElement('button');
+
+    removeButton.innerHTML = "remove";
+
+    removeButton.setAttribute( "type" , "button");
+
+    removeButton.addEventListener("click", function(event){
+    
+    let entry = removeButton.parentNode;
+
+    entry.remove();
+
+
+    });
+
+    newMessage.appendChild(removeButton);
+
+    messageList.appendChild(newMessage);
+
+    event.target.reset();
+});
+
 
