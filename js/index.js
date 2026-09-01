4b45b7bb820c012108e0528980139e838381ae6f
Morgan Wilcox <31056643+mwilcox6@users.noreply.github.com>
2026-09-01 15:09:51 -0400

    API fetch completed

--- js/index.js
+++ js/index.js
@@ -68,24 +68,40 @@ messageForm.addEventListener("submit" , function (event)
     event.target.reset();
 });
 
-fetch("https://api.github.com/users/mwilcox6/repos")
-.then(function(response) {
-
-    if (!response.ok) {
+fetch ("https://api.github.com/users/mwilcox6/repos") 
+.then(function(response){
+ 
+    if (!response.ok){
         throw new Error(response.status);
     }
-
+    
     return response.json();
 
 })
-.then(function(data) {
+
+.then(function (data){
     const repositories = data;
     console.log(repositories);
 
+    let projectSection = document.getElementById('Projects');
+
+let projectList = projectSection.querySelector('ul');
+
+for (let i = 0; i < repositories.length ; i++ ) {
+
+    const project = document.createElement('li');
+
+    project.innerText = repositories[i].name;
+
+    projectList.appendChild(project);
+}
+
 })
+
 .catch(function(error) {
 
-    console.error(error);
+console.error(error)
 
 });
+
 
